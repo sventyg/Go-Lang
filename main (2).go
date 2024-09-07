4336b5b83c73a91cec6cc8a2f50a73878c66f0f9
Griffin Blaise Sventy <77992348+sventyg@users.noreply.github.com>
2024-09-07 17:14:10 -0400

    Implements Luhn's algorithm
    Go code implements Luhn's algorithm to validate a credit card number.

--- main (2).go
+++ main (2).go
@@ -1,45 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 
-	var letter string
+	var i int
+	var j int
+	var k int
+	var l int
+	var z int
+	var h int
+	var r int
+	var ds int
+	var f int
+	var num string
+	var let1 [100]string
+	var let2 [100]string
+	var n1 [100]int
 	
-	fmt.Println("Enter letter grade: ")
-	fmt.Scanf("%s",&letter)
-	
-	runes := []rune(letter)
-	sub1 := string(runes[0:1])
-	sub2 := string(runes[1:2])
-	
-	if sub2 == "+" {
-		add := 0.3
-	}else{
-	if sub2 == "-" {
-		add := -0.3
-	}}else{
-	num := 0
+	fmt.Print("Credit card number: ")
+	fmt.Scan(&num)
+	runes := []rune(num)
+	leng := len(num)
+
+	l = 1
+	for k = 0; k < leng; k++ {
+		ter2 := string(runes[k:l])
+		let2[k] = ter2
+		l += 2
+		k++
+	}
+	j = leng
+	for i = leng - 1; i > 0; i-- {
+		ter1 := string(runes[i:j])
+		l1[i] = ter1
+		i--
+		j -= 2
+	}
+	for z = 0; z < leng; a++ {
+		if let1[z] != "" {
+			rs, err1 := strconv.Atoi(l1[z])
+			r += rs
+			if err1 != nil {
+				fmt.Print("")
+		}}
+	}
+	for h = 0; h < leng-1; h++ {
+		if let2[h] != "" {
+			dn, err2 := strconv.Atoi(l2[h])
+			n1[h] = dn
+			if err2 != nil {
+				fmt.Print("")
+		}}
+		h++
 	}
 	
-	if sub1 == "A" {
-		num := 4
-	}else{
-	if sub1 == "B" {
-		num := 3
-	}else{
-	if sub1 == "C" {
-		num := 2
-	}else{
-	if sub1 == "D" {
-		num := 1
-	}else{
-	if sub1 == "F" {
-		num := 0	
-		add := 0
-	}
+	d1 := n1[0]
+	d2 := n1[2]
+	d3 := n1[4]
+	d4 := n1[6]
+	db1 := d1 * 2
+	db2 := d2 * 2
+	db3 := d3 * 2
+	db4 := d4 * 2
+	p1 := db1 / 10
+	v1 := db1 % 10
+	p2 := db2 / 10
+	v2 := db2 % 10
+	p3 := db3 / 10
+	v3 := db3 % 10
+	p4 := db4 / 10
+	v4 := db4 % 10
+	ds = p1 + p2 + p3 + p4 + v1 + v2 + v3 + v4
+	f = r + ds
 	
-	fmt.Println("Number grade: ")
-	fmt.Print(num + add)
+	if fs%10 == 0 {
+		fmt.Println("Valid")
+	} else {
+		fmt.Println("Invalid")
+	}
 
 }
+
+
